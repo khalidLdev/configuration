eb5c53d57a2a3f126f008af425b9718074084e4b
Yassine EL OUARDI <yelouardi@sqli.com>
2019-12-27 11:08:46 +0100

    first commit

---
+++ src/test/java/com/humanup/matrix/management/server/configuration/ConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanup.matrix.management.server.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
