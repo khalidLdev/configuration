09a690a804b3d0878d3fbcd9878f7275662f4476
Yassine EL OUARDI <yelouardi@sqli.com>
2020-04-28 19:56:49 +0200

    Add New Properties File

--- src/main/java/com/humanup/matrix/management/server/configuration/ConfigurationApplication.java
+++ src/main/java/com/humanup/matrix/management/server/configuration/ConfigurationApplication.java
@@ -3,7 +3,6 @@ package com.humanup.matrix.management.server.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
